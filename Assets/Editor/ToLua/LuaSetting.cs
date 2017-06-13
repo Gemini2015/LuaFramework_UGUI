59e777eb21e576de77e5e01e5772ab97bc189e18
Chris Cheng <gemini2015@hotmail.com>
2017-06-13 21:18:33 +0800

    整理代码

---
+++ Assets/Editor/ToLua/LuaSetting.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using LuaInterface;
+
+using BindType = ToLuaMenu.BindType;
+using UnityEngine.UI;
+
+public static class LuaSetting
+{
+    public static string saveDir = Application.dataPath + "/Scripts/ToLua/Source/Generate/";
+    public static string toluaBaseType = Application.dataPath + "/Scripts/ToLua/BaseType/";
+
+    //导出时强制做为静态类的类型(注意customTypeList 还要添加这个类型才能导出)
+    //unity 有些类作为sealed class, 其实完全等价于静态类
+    public static List<Type> staticClassTypes = new List<Type>
+    {        
+        typeof(UnityEngine.Application),
+        typeof(UnityEngine.Time),
+        typeof(UnityEngine.Screen),
+        typeof(UnityEngine.SleepTimeout),
+        typeof(UnityEngine.Input),
+        typeof(UnityEngine.Resources),
+        typeof(UnityEngine.Physics),
+        typeof(UnityEngine.RenderSettings),
+        typeof(UnityEngine.QualitySettings),
+        typeof(UnityEngine.GL),
+    };
+
+    //附加导出委托类型(在导出委托时, customTypeList 中牵扯的委托类型都会导出， 无需写在这里)
+    public static DelegateType[] customDelegateList = 
+    {        
+        _DT(typeof(Action)),                
+        _DT(typeof(UnityEngine.Events.UnityAction)),
+        _DT(typeof(System.Predicate<int>)),
+        _DT(typeof(System.Action<int>)),
+        _DT(typeof(System.Comparison<int>)),
+    };
+
+    //在这里添加你要导出注册到lua的类型列表
+    public static BindType[] customTypeList =
+    {
+        _GT(typeof(Debugger)).SetNameSpace(null),        
+
+#if USING_DOTWEENING
+        _GT(typeof(DG.Tweening.DOTween)),
+        _GT(typeof(DG.Tweening.Tween)).SetBaseType(typeof(System.Object)).AddExtendType(typeof(DG.Tweening.TweenExtensions)),
+        _GT(typeof(DG.Tweening.Sequence)).AddExtendType(typeof(DG.Tweening.TweenSettingsExtensions)),
+        _GT(typeof(DG.Tweening.Tweener)).AddExtendType(typeof(DG.Tweening.TweenSettingsExtensions)),
+        _GT(typeof(DG.Tweening.LoopType)),
+        _GT(typeof(DG.Tweening.PathMode)),
+        _GT(typeof(DG.Tweening.PathType)),
+        _GT(typeof(DG.Tweening.RotateMode)),
+        _GT(typeof(Component)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        _GT(typeof(Transform)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        _GT(typeof(Light)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        _GT(typeof(Material)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        _GT(typeof(Rigidbody)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        _GT(typeof(Camera)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        _GT(typeof(AudioSource)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        //_GT(typeof(LineRenderer)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),
+        //_GT(typeof(TrailRenderer)).AddExtendType(typeof(DG.Tweening.ShortcutExtensions)),    
+#else
+                                         
+        _GT(typeof(Component)),
+        _GT(typeof(Transform)),
+        _GT(typeof(Material)),
+        _GT(typeof(Light)),
+        _GT(typeof(Rigidbody)),
+        _GT(typeof(Camera)),
+        _GT(typeof(AudioSource)),
+#endif
+      
+        _GT(typeof(Behaviour)),
+        _GT(typeof(MonoBehaviour)),        
+        _GT(typeof(GameObject)),
+        _GT(typeof(TrackedReference)),
+        _GT(typeof(Application)),
+        _GT(typeof(Physics)),
+        _GT(typeof(Collider)),
+        _GT(typeof(Time)),        
+        _GT(typeof(Texture)),
+        _GT(typeof(Texture2D)),
+        _GT(typeof(Shader)),        
+        _GT(typeof(Renderer)),
+        _GT(typeof(WWW)),
+        _GT(typeof(Screen)),        
+        _GT(typeof(CameraClearFlags)),
+        _GT(typeof(AudioClip)),        
+        _GT(typeof(AssetBundle)),
+        _GT(typeof(ParticleSystem)),
+        _GT(typeof(AsyncOperation)).SetBaseType(typeof(System.Object)),        
+        _GT(typeof(LightType)),
+        _GT(typeof(SleepTimeout)),
+#if UNITY_5_3_OR_NEWER && !UNITY_5_6_OR_NEWER
+        _GT(typeof(UnityEngine.Experimental.Director.DirectorPlayer)),
+#endif
+        _GT(typeof(Animator)),
+        _GT(typeof(Input)),
+        _GT(typeof(KeyCode)),
+        _GT(typeof(SkinnedMeshRenderer)),
+        _GT(typeof(Space)),      
+       
+        _GT(typeof(Animation)),        
+        _GT(typeof(AnimationClip)).SetBaseType(typeof(UnityEngine.Object)),        
+        _GT(typeof(AnimationState)),
+        _GT(typeof(AnimationBlendMode)),
+        _GT(typeof(QueueMode)),  
+        _GT(typeof(PlayMode)),
+        _GT(typeof(WrapMode)),
+      
+    };
+
+    public static List<Type> dynamicList = new List<Type>()
+    {
+        typeof(MeshRenderer),
+
+        typeof(BoxCollider),
+        typeof(MeshCollider),
+        typeof(SphereCollider),
+        typeof(CharacterController),
+        typeof(CapsuleCollider),
+
+        typeof(Animation),
+        typeof(AnimationClip),
+        typeof(AnimationState),
+
+        typeof(BlendWeights),
+        typeof(RenderTexture),
+        typeof(Rigidbody),
+    };
+
+    //重载函数，相同参数个数，相同位置out参数匹配出问题时, 需要强制匹配解决
+    //使用方法参见例子14
+    public static List<Type> outList = new List<Type>()
+    {
+        
+    };
+
+    static BindType _GT(Type t)
+    {
+        return new BindType(t);
+    }
+
+    static DelegateType _DT(Type t)
+    {
+        return new DelegateType(t);
+    }    
+}
---
+++ Assets/Scripts/ToLua/Misc/LuaClient.cs
@@ -0,0 +1,133 @@
+﻿/*
+Copyright (c) 2015-2016 topameng(topameng@qq.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using UnityEngine;
+using LuaInterface;
+
+public class LuaClient : MonoBehaviour
+{
+    public static LuaClient Instance
+    {
+        get;
+        protected set;
+    }
+
+    protected LuaState luaState = null;
+    protected LuaLooper loop = null;
+    
+    protected virtual void LoadLuaFiles()
+    {
+        OnLoadFinished();
+    }
+
+    protected virtual void OpenLibs()
+    {
+        luaState.OpenLibs(LuaDLL.luaopen_pb);
+        luaState.OpenLibs(LuaDLL.luaopen_struct);
+        luaState.OpenLibs(LuaDLL.luaopen_lpeg);
+#if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
+        luaState.OpenLibs(LuaDLL.luaopen_bit);
+#endif
+        
+    }
+        
+    //cjson 比较特殊，只new了一个table，没有注册库，这里注册一下
+    protected void OpenCJson()
+    {
+        luaState.LuaGetField(LuaIndexes.LUA_REGISTRYINDEX, "_LOADED");
+        luaState.OpenLibs(LuaDLL.luaopen_cjson);
+        luaState.LuaSetField(-2, "cjson");
+
+        luaState.OpenLibs(LuaDLL.luaopen_cjson_safe);
+        luaState.LuaSetField(-2, "cjson.safe");                               
+    }
+
+    protected void StartLooper()
+    {
+        loop = gameObject.AddComponent<LuaLooper>();
+        loop.luaState = luaState;
+    }
+
+    protected virtual void Bind()
+    {        
+        LuaBinder.Bind(luaState);
+        LuaCoroutine.Register(luaState, this);
+    }
+
+    protected void Init()
+    {
+        luaState = new LuaState();
+        OpenLibs();
+        luaState.LuaSetTop(0);
+        Bind();
+        LoadLuaFiles();    
+    }
+
+    protected void Awake()
+    {
+        Instance = this;
+        Init();
+    }
+
+    protected virtual void OnLoadFinished()
+    {
+        luaState.Start();
+        StartLooper();
+    }
+    
+    public virtual void Destroy()
+    {
+        if (luaState != null)
+        {
+            LuaState state = luaState;
+            luaState = null;
+            
+            if (loop != null)
+            {
+                loop.Destroy();
+                loop = null;
+            }
+
+            state.Dispose();            
+            Instance = null;
+        }
+    }
+
+    protected void OnDestroy()
+    {
+        Destroy();
+    }
+
+    protected void OnApplicationQuit()
+    {
+        Destroy();
+    }
+
+    public static LuaState GetMainState()
+    {
+        return Instance.luaState;
+    }
+
+    public LuaLooper GetLooper()
+    {
+        return loop;
+    }
+}
